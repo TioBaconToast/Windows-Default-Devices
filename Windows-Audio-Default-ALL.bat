231fd2f9fcd2aed9230228eac1392fceffe4c089
Tio <115176292+TioBaconToast@users.noreply.github.com>
2022-10-14 12:16:29 -0400

    Add files via upload

---
+++ Windows-Audio-Default-ALL.bat
@@ -0,0 +1,25 @@
+chcp 65001
+@echo off
+
+::========= SET DEVICE NAME ===========::
+Set theDefaultOutput="REPLACE WITH DEVICE NAME"  ::(Replace with MAIN MIC device)
+
+Set theDefaultOutputCOMS="REPLACE WITH DEVICE NAME" ::(Device for Calls and Coms) 
+
+Set theDeviceRecorder="REPLACE WITH DEVICE NAME"  ::(Device for default PC audio)
+
+Set theDeviceRecorderComs="REPLACE WITH DEVICE NAME" ::(MIC device for Calls and Coms) 
+
+
+::===========================::
+::========= CODE ============::
+
+nircmd setdefaultsounddevice %theDefaultOutput% 1
+
+nircmd setdefaultsounddevice %theDefaultOutputCOMS% 2
+
+nircmd setdefaultsounddevice %theDeviceRecorder% 1
+
+nircmd setdefaultsounddevice %theDeviceRecorder% 2
+
+exit /b
